9be1f3f0715d0be2ca51d4a2a2f237bc125f12b6
neil patel <neil@myindianweddingapp.com>
2015-03-02 10:40:53 +0000

    added Q&A form

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,28 @@
+class AnswersController < Applicationcontroller
+
+def create
+
+	@question = Question.find(params[:question_id])
+	@answer = @question.answers.build(answers_params)
+	@answer.user = current_user
+	@answer.save
+
+	redirect_to questions_url
+
+end
+
+def destroy
+
+	@question = Question.find(params[:question_id])
+	@answer = @question.answers.find(params[:id]).destroy
+	redirect_to questions_url
+
+end
+
+private
+
+	def answers_params
+		params.require(:answer).permit(:content)
+	end
+
+end
